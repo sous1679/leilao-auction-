820f41378400a70b178c03e017c26beb54968a43
Matheus Silva Freitas <msformigteen@live.com>
2018-01-20 13:08:38 -0300

    Form Cadastro Finalizado

---
+++ src/Cadastro.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Cadastro {
+    private JPanel Tela;
+    private JTextField Texto;
+    private JLabel NameLabel;
+    private JTextField NameField;
+    private JPanel NamePanel;
+    private JPanel EmailPanel;
+    private JLabel EmailLabel;
+    private JTextField EmalField;
+    private JPanel SenhaPanel;
+    private JPanel ConfirmPanel;
+    private JLabel ConfirmLabel;
+    private JTextField ConfirmField;
+    private JLabel SenhaLabel;
+    private JTextField SenhaField;
+    private JButton Confirm;
+}
